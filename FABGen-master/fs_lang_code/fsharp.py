730e0718d7045081d2d423ba53ebaec68d30b91b
Gregory-Pagnoux <114397869+Gregory-Pagnoux@users.noreply.github.com>
2023-01-09 16:38:30 +0100

    update

--- FABGen-master/fs_lang_code/fsharp.py
+++ FABGen-master/fs_lang_code/fsharp.py
@@ -19,3 +19,60 @@ class FSharpTypeConverterCommon(gen.TypeConverter):
     
     def get_type_glue(self, gen, module_name):
         return ''
+
+class FSharpTypeConverter(FSharpTypeConverterCommon):
+    def __init__(self, type, to_c_storage_type=None, bound_name=None, from_c_storage_type=None, needs_c_storage_class=False):
+        super().__init__(type, to_c_storage_type, bound_name, from_c_storage_type, needs_c_storage_class)
+    
+    def get_type_api(self, module_name):
+        return ''
+    
+    def to_c_call(self, in_var, out_var_p):
+        return ''
+    
+    def from_c_call(self, out_var, expr, ownership):
+        return ''
+    
+    def check_call(self, in_var):
+        return ''
+    
+    def get_type_glue(self, gen, module_name):
+        return ''
+
+class FSharpTypeConverterStruct(FSharpTypeConverterCommon):
+    def __init__(self, type, to_c_storage_type=None, bound_name=None, from_c_storage_type=None, needs_c_storage_class=False):
+        super().__init__(type, to_c_storage_type, bound_name, from_c_storage_type, needs_c_storage_class)
+    
+    def get_type_api(self, module_name):
+        return ''
+    
+    def to_c_call(self, in_var, out_var_p):
+        return ''
+    
+    def from_c_call(self, out_var, expr, ownership):
+        return ''
+    
+    def check_call(self, in_var):
+        return ''
+    
+    def get_type_glue(self, gen, module_name):
+        return ''
+
+class FSharpTypeConverterEnum(FSharpTypeConverterCommon):
+    def __init__(self, type, to_c_storage_type=None, bound_name=None, from_c_storage_type=None, needs_c_storage_class=False):
+        super().__init__(type, to_c_storage_type, bound_name, from_c_storage_type, needs_c_storage_class)
+    
+    def get_type_api(self, module_name):
+        return ''
+    
+    def to_c_call(self, in_var, out_var_p):
+        return ''
+    
+    def from_c_call(self, out_var, expr, ownership):
+        return ''
+    
+    def check_call(self, in_var):
+        return ''
+    
+    def get_type_glue(self, gen, module_name):
+        return ''
