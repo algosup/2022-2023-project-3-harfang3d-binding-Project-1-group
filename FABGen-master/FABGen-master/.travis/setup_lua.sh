f62f7a5711930e0d0ca5bc4e62f8b6caf74bf000
TheoTr <71769486+TheoTr@users.noreply.github.com>
2023-01-09 13:52:19 +0100

    Restruct

--- FABGen-master/FABGen-master/.travis/setup_lua.sh
+++
@@ -1,9 +0,0 @@
-curl http://www.lua.org/ftp/lua-5.3.2.tar.gz | tar xz
-pushd lua-5.3.2
-
-LUA_HOME_DIR=$TRAVIS_BUILD_DIR/install/lua
-
-make linux -j
-make INSTALL_TOP="$LUA_HOME_DIR" install
-
-popd
