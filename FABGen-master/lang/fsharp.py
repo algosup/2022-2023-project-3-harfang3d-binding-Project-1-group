c1b5114f22ce0e998a21e8208e4551764479a5ca
Gregory-Pagnoux <114397869+Gregory-Pagnoux@users.noreply.github.com>
2023-01-11 16:56:40 +0100

    starting

--- FABGen-master/lang/fsharp.py
+++ FABGen-master/lang/fsharp.py
@@ -1,1 +1,19 @@
+# Harfang - The Fabulous binding Generator for CPython and FSharp
+
+import os
+
 import gen
+
+import lib
+
+class FSharpGenerator(gen.Generator):
+    def __init__(self, module, output_dir, output_file, output_namespace):
+        gen.Generator.__init__(self, module, output_dir, output_file, output_namespace)
+
+        self.output_dir = output_dir
+        self.output_file = output_file
+        self.output_namespace = output_namespace
+
+        self.output_file = os.path.join(self.output_dir, self.output_file)
+
+        self.output = open(self.output_file, "w")
