9085ebcde0bf038e2873168722cc2a582ca67737
Nicolas MIDA <nicolas.mida@algosup.com>
2023-01-31 09:47:15 +0100

    Added v3 functions

--- TestFsBind/vectors.cpp
+++ TestFsBind/vectors.cpp
@@ -76,22 +76,24 @@ extern "C"
     }
 
     Vector3::Vector3(double x, double y, double z) : x(x), y(y), z(z) {}
-    double Vector3::distanceTo(Vector3 pos) {
-        return sqrt((pos.y - y) * (pos.y - y) + (pos.x - x) * (pos.x - x) + (pos.z - z) * (pos.z - z));
-    }
-    void Vector3::vectorMovement(double plusx, double plusy, double plusz) {
-        x += plusx;
-        y += plusy;
-        z += plusz;
-    }
 
-    Vector3 Vector3::midpoint(Vector3 pos) {
-        return Vector3((x + pos.x) / 2, (y + pos.y) / 2, (z + pos.z) / 2);
-    }
+    // double Vector3::distanceTo(Vector3 pos) {
+    //     return sqrt((pos.y - y) * (pos.y - y) + (pos.x - x) * (pos.x - x) + (pos.z - z) * (pos.z - z));
+    // }
+    
+    // void Vector3::vectorMovement(double plusx, double plusy, double plusz) {
+    //     x += plusx;
+    //     y += plusy;
+    //     z += plusz;
+    // }
 
-    double Vector3::percentDistance(Vector3 pos, double percentOfDistance) {
-        return distanceTo(pos) * percentOfDistance;
-    }
+    // Vector3 Vector3::midpoint(Vector3 pos) {
+    //     return Vector3((x + pos.x) / 2, (y + pos.y) / 2, (z + pos.z) / 2);
+    // }
+
+    // double Vector3::percentDistance(Vector3 pos, double percentOfDistance) {
+    //     return distanceTo(pos) * percentOfDistance;
+    // }
 
     double v3distanceTo(double x1, double y1, double z1) {
         return sqrt(pow(x1, 2) + pow(y1, 2) + pow(z1, 2));
@@ -99,6 +101,16 @@ extern "C"
 
     double v3percentDistance(double x, double y, double z, double percentOfDistance) {
         return v3distanceTo(x, y, z) * percentOfDistance;
+    }
+
+    double v3vectorMovement(Vector3 v3, double plusx, double plusy, double plusz) {
+        v3.x += plusx;
+        v3.y += plusy;
+        v3.z += plusz;
+    }
+
+    Vector3 v3midpoint(Vector3 v3, Vector3 pos) {
+        return Vector3((v3.x + pos.x) / 2, (v3.y + pos.y) / 2, (v3.z + pos.z) / 2);
     }
 
 }
