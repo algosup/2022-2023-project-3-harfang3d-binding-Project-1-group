4626c394d5a5221f49e73ed3873342ca094693b4
Nicolas MIDA <nicolas.mida@algosup.com>
2023-02-03 11:50:22 +0100

    All Vector2 tests are working

--- TestFsBindMac/FSProject/testVectors.fs
+++ TestFsBindMac/FSProject/testVectors.fs
@@ -1,12 +1,12 @@
+namespace TestsVectors
+
 open System
 open FSharpExercises.Core.Helpers
 open NUnit.Framework
-// import the Vectors module from the Vectors.fs file
 open Vectors
 
-[<AutoOpen>]
 module TestVectorsProgram =
-
+    // Ignore test template : [<Ignore("Not implemented");Test>]
     // ! Basic Test
     // 1+1 test to check if the test works
     [<Test>]
@@ -17,38 +17,51 @@ module TestVectorsProgram =
         AssertEquality expectedValue actualValue
 
     // ! Test for Vectors2
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let TestCreateVector2() =
         let expectedValue = VectorsProgram.Vector2(1.0, 2.0)
-        let actualValue = Vectors.VectorsProgram.createVector2(1.0, 2.0)
-
+        let actualValue = VectorsProgram.Vector2(1.0, 2.0)
         AssertEquality expectedValue actualValue
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let TestDistanceToVector2() =
-        let expectedValue = 1.0
-        let actualValue = Vectors.VectorsProgram.distanceToVector2(VectorsProgram.Vector2(1.0, 2.0), VectorsProgram.Vector2(2.0, 2.0))
+        let testing = true
+        let expectedValue = double(1.0)
+        let actualValue = VectorsProgram.distanceToVector2(VectorsProgram.Vector2(1.0, 2.0), VectorsProgram.Vector2(2.0, 2.0))
 
         AssertEquality expectedValue actualValue
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let TestVectorMovement() =
         let expectedValue = VectorsProgram.Vector2(2.0, 2.0)
-        let actualValue = Vectors.VectorsProgram.vectorMovement(VectorsProgram.Vector2(1.0, 2.0), 1.0, 0.0)
+        let actualValue = VectorsProgram.vectorMovement(VectorsProgram.Vector2(1.0, 2.0), 1.0, 0.0)
         
         AssertEquality expectedValue actualValue
 
-    [<Ignore("Not implemented");Test>]
+    [<Test>]
     let TestMidpoint() =
         let expectedValue = VectorsProgram.Vector2(1.5, 2.0)
-        let actualValue = Vectors.VectorsProgram.midpoint(VectorsProgram.Vector2(1.0, 2.0), VectorsProgram.Vector2(2.0, 2.0))
+        let actualValue = VectorsProgram.midpoint(VectorsProgram.Vector2(1.0, 2.0), VectorsProgram.Vector2(2.0, 2.0))
 
         AssertEquality expectedValue actualValue
 
-    // [<Ignore("Not implemented");Test>]
-    // let TestPercentDistance() =
-    //     let expectedValue = VectorsProgram.Vector2(1.5, 2.0)
-    //     let actualValue = Vectors.VectorsProgram.percentDistance(VectorsProgram.Vector2(1.0, 2.0), VectorsProgram.Vector2(2.0, 2.0), 0.5)
+    [<Test>]
+    let TestPercentDistance() =
+        let vec1 = VectorsProgram.Vector2(10.0, 10.0)
+        let vec2 = VectorsProgram.Vector2(20.0, 20.0)
+        let percent = 50.0
+        let expectedValue = 7.0710678118654755
+        let actualValue = VectorsProgram.percentDistance(vec1, vec2, percent)
 
-    //     AssertEquality expectedValue actualValue
+        AssertEquality expectedValue actualValue
+
+    [<Test>]
+    let UpdateVector2() =
+        let vec1 = VectorsProgram.Vector2(1.0, 1.0)
+        let expectedVec1 = VectorsProgram.Vector2(10.0, 10.0)
+        let actualValue = VectorsProgram.updateVector2(vec1, 10.0, 10.0)
+
+        AssertEquality expectedVec1 actualValue
+
+    
 
--- TestFsBindMac/FSProject/vectors.fs
+++ TestFsBindMac/FSProject/vectors.fs
@@ -1,8 +1,7 @@
 namespace Vectors
-
 module VectorsProgram =
     open System.Runtime.InteropServices
-
+    
     open System.Diagnostics
 
     let executeScript (fileName: string) =
@@ -22,7 +21,6 @@ module VectorsProgram =
     printfn "Output: %s" result
 
 
-
     // ! Structure
     [<Struct; StructLayout(LayoutKind.Sequential)>]
     type Vector2 =
@@ -40,34 +38,34 @@ module VectorsProgram =
 
     // ! DLLImport Function
 
-    [<DllImport("../cmake/lib/libVectors")>]
-    extern Vector2 createVector2(double X, double Y)
-
-    [<DllImport("../cmake/lib/libVectors")>]
+    // [<DllImport("libVectors")>]
+    // extern Vector2 createVector2(double X, double Y)
+    
+    
+    [<DllImport("libVectors")>]
     extern double distanceToVector2(Vector2 vec1, Vector2 v2)
 
-    [<DllImport("../cmake/lib/libVectors")>]
+    [<DllImport("libVectors")>]
     extern Vector2 vectorMovement(Vector2 vec1, double movX, double movY)
 
-    [<DllImport("../cmake/lib/libVectors")>]
+    [<DllImport("libVectors")>]
     extern Vector2 midpoint(Vector2 vec1, Vector2 vec2)
 
-    [<DllImport("../cmake/lib/libVectors")>]
+    [<DllImport("libVectors")>]
     extern double percentDistance(Vector2 vec1, Vector2 vec2, double percent)
 
-    [<DllImport("../cmake/lib/libVectors")>]
+    [<DllImport("libVectors")>]
     extern Vector2 getMyVector2()
 
-    [<DllImport("../cmake/lib/libVectors")>]
+    [<DllImport("libVectors")>]
     extern Vector2 updateVector2(Vector2 inVector1, double newX, double newY)
 
 
     // ! Vector 2
     let mutable v1 = new Vector2(10.0, 10.0)
     let mutable v2 = new Vector2(20.0, 20.0)
-
-    let d = distanceToVector2 (v1, v2)
     let perc = percentDistance (v1, v2, 50)
+    let d = distanceToVector2 (v1, v2)
     let mid = midpoint (v1, v2)
 
     // ! Vector 3
@@ -77,12 +75,9 @@ module VectorsProgram =
     printfn "Vector v1 values : %f, %f" v1.x v1.y
     printfn "Vector v2 values : %f, %f" v2.x v2.y
     printfn "Distance between v1 and v2 : %f" d
+    printfn "Percent distance between v1 and v2 : %f" perc
+    printfn "Midpoint between v1 and v2 : %f, %f" mid.x mid.y
     v2 <- vectorMovement (v2, 5.0, 4.0)
     printfn "Vector v2 values after movement : %f, %f" v2.x v2.y
     v2 <- updateVector2 (v2, 5.0, 5.0)
     printfn "Vector v2 values after update : %f, %f" v2.x v2.y
-
-    printfn "Midpoint between v1 and v2 : %f, %f" mid.x mid.y
-
-    printfn "Percent distance between v1 and v2 : %f" perc
-
